4aab0912290dc030e09f73c4cbb8171669097301
Leandro Celle (WPP Group USA LLC) <leangerman13@gmail.com>
2025-01-03 22:01:27 -0300

    Removing add files

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import app  # Ajusta esta línea si tu aplicación está en otro archivo
-
-os.environ.setdefault("FLASK_APP", "app")
-os.environ.setdefault("FLASK_ENV", "production")
-
-if __name__ == "__main__":
-    app.run()
