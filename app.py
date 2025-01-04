2b0357babd606fbb7c292ad2c1f9a46188904f57
Leandro Celle (WPP Group USA LLC) <leangerman13@gmail.com>
2025-01-04 20:12:17 -0300

    Modify BRK-A

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def predict_price(ticker, start_date="2024-01-01"):
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "NVDA", "BRK.A", "AVGO", "DIS", "V", "LLY", "WMT", "JPM", "MA", "XOM", ]
+    tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "NVDA", "BRK-A", "AVGO", "DIS", "V", "LLY", "WMT", "JPM", "MA", "XOM", ]
 
     predictions = []
     for ticker in tickers:
