3e46bbe62a355bbd170942c4d12a57bd0fa5c372
Leandro Celle (WPP Group USA LLC) <leangerman13@gmail.com>
2025-01-03 21:42:31 -0300

    modify back

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
-from scripts.app import app  # Ajusta esta línea si tu aplicación está en otro archivo
+from app import app  # Ajusta esta línea si tu aplicación está en otro archivo
 
-os.environ.setdefault("FLASK_APP", "scripts.app")
+os.environ.setdefault("FLASK_APP", "app")
 os.environ.setdefault("FLASK_ENV", "production")
 
 if __name__ == "__main__":
