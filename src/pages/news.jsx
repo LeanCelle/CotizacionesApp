1f3b3c8a51114b461eb9b7d0c9da4bd13ce9fa17
Leandro Celle (WPP Group USA LLC) <leangerman13@gmail.com>
2025-01-12 01:32:27 -0300

    Correcting backend

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -9,7 +9,7 @@ const News = () => {
   useEffect(() => {
     // Obtener las noticias desde el backend
     axios
-      .get("http://localhost:8000/news") // Ajusta la URL si está desplegado
+      .get(`${process.env.REACT_APP_NEWS_URL}/news`) // Ajusta la URL si está desplegado
       .then((response) => {
         setNews(response.data.news || []);
         setLoading(false);
